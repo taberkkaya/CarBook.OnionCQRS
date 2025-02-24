e6de895878ab20522e09cdec8d37fb99676fd0c0
taberkkaya <taberkkaya@gmail.com>
2025-02-24 06:54:03 +0300

    mediator implementation for service

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ServiceHandlers/UpdateServiceCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Commands.ServiceCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ServiceHandlers;
+
+public class UpdateServiceCommandHandler : IRequestHandler<UpdateServiceCommand>
+{
+    private readonly IRepository<Service> _repository;
+
+    public UpdateServiceCommandHandler(IRepository<Service> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(UpdateServiceCommand request, CancellationToken cancellationToken)
+    {
+        var value = await _repository.GetByIdAsync(request.Id);
+
+        value!.Title = request.Title;
+        value.Description = request.Description;
+        value.IconUrl = request.IconUrl;
+        value.UpdatedDate = DateTime.Now;
+
+        await _repository.UpdateAsync(value);
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Queries/ServiceQueries/GetServiceByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Application.Features.Mediator.Results.ServiceResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Queries.ServiceQueries;
+
+public class GetServiceByIdQuery : IRequest<GetServiceByIdQueryResult>
+{
+    public int Id { get; set; }
+
+    public GetServiceByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
