45aa0cd30095e668db6eb90f3ffe58168ec7dcb4
taberkkaya <taberkkaya@gmail.com>
2025-02-25 06:25:23 +0300

    ui about page done

---
+++ Frontends/CarBook.Dto/TestimonialDtos/TestimonialResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBook.Dto.TestimonialDtos;
+
+public class TestimonialResultDto
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public string imageUrl { get; set; }
+}
